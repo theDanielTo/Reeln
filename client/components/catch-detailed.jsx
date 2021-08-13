e726723b33d6bbc418109c6dda05cb4ce5707274
theDanielTo <dto1989@gmail.com>
2021-08-13 12:09:43 -0700

    Catch modal renders, closes on click

--- client/components/catch-detailed.jsx
+++ client/components/catch-detailed.jsx
@@ -3,16 +3,21 @@ import React from 'react';
 export default function CatchDetailed(props) {
   const { hidden, onBgClick } = props;
   const { photo, firstName, lastName, dateCaught, weight } = props.catchDetailed;
+  const modalVis = (hidden)
+    ? 'hidden'
+    : '';
   return (
-    <div className="catch-detailed-bg"
-      hidden={hidden}
+    <div className={'modal-bg flex-center ' + modalVis}
       onClick={onBgClick}>
       <div className="catch-detailed-modal">
-        <button onClick={onBgClick}>close</button>
+        <i className="far fa-window-close"
+          onClick={onBgClick} />
+        <div className="catch-detailed-text">
+          <p>{'Caught by: ' + firstName + ' ' + lastName}</p>
+          <p>{'On ' + dateCaught}</p>
+          <p>{'Weight: ' + weight + ' pounds'}</p>
+        </div>
         <img className="catch-detailed-img" src={'./images/' + photo} alt="Photo of fish" />
-        <p className="catch-detailed-text">{firstName + ' ' + lastName}</p>
-        <p className="catch-detailed-text">{'Caught on: ' + dateCaught}</p>
-        <p className="catch-detailed-text">{'Weight: ' + weight + ' pounds'}</p>
       </div>
     </div>
   );
