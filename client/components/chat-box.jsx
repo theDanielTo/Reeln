e5a6d5acecfa01b14908e4fcffac2e26cfcb68c4
theDanielTo <dto1989@gmail.com>
2021-08-14 11:39:44 -0700

    Styled chatbox. Handled input changes with usestate

--- client/components/chat-box.jsx
+++ client/components/chat-box.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Chatbox(props) {
-  function displayMessage(msg) {
+  const [messages, setMessages] = useState(['test', 'msg1', 'msg2']);
+  const [chatInput, setChatInput] = useState('');
 
+  function handleChatInputChange(e) {
+    setChatInput(chatInput => e.target.value);
+  }
+
+  function displayMsgs() {
+    return messages.map(msg => {
+      return (
+        <p key={msg}>{msg}</p>
+      );
+    });
+  }
+
+  function sendMsg(e) {
+    e.preventDefault();
+    setMessages(prevMsgs => [...prevMsgs, chatInput.value]);
   }
 
   return (
     <div className="chat-container">
       <div className="messages-container">
-
+        {displayMsgs()}
       </div>
-      <form onSubmit={displayMessage}>
-        <label htmlFor="message-input"></label>
-        <input type="text" id="message-input"/>
-        <button type="submit" id="send-button">Send</button>
+      <form onSubmit={sendMsg} className="chat-input-row">
+        <label htmlFor="chatInput">
+          <i className="fas fa-plus-circle" />
+        </label>
+        <input type="text" className="border-none"
+          id="chat-input" name="chatInput"
+          onChange={handleChatInputChange} />
+        <button type="submit"
+          id="chat-send-button" className="border-none">
+          Send
+        </button>
       </form>
     </div>
   );
